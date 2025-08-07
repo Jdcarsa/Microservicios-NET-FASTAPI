20269522f9e236345e0631336d922d8789286bd8
Jdcarsa <jdcardenasvld@hotmail.com>
2025-08-07 17:08:03 -0500

    corregir git

--- CoursesWebApplication/CoursesWeb.Apphost/Program.cs
+++
@@ -1,7 +0,0 @@
-var builder = DistributedApplication.CreateBuilder(args);
-
-var userService = builder.AddProject<Projects.User_Service>("apiservice-user");
-var coursesService = builder.AddProject<Projects.Courses_Service>("apiservice-courses");
-
-
-builder.Build().Run();
